63f47a5912124c1607179fd3e2673b8e6ffc9629
paulhan <zhangyu.han@outlook.com>
2017-10-24 23:54:45 +0800

    数组去重测试

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
+var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
+var a, b, c;
+a = [1, 2, 3, 2, 4, 4, 4, 3, 5, 5, 5, 6];
+b = [1, 2, 3, 2, 4, 4, 4, 3, 5, 5, 5, 6];
+c = [1, 2, 3, 2, 4, 4, 4, 3, 5, 5, 5, 6];
+
+var uniqueFor = function(arr) {
+	for (var i = 0; i < arr.length - 1; i++) {
+		var item = arr[i];
+		for (var j = i + 1; j < arr.length; j++) {
+			item === arr[j] && (arr.splice(j, 1), j--);
+		}
+	};
+	return arr;
+};
+var uniqueObject = function(arr) {
+	var v, r = [],
+	o = {};
+	for (var i = 0;
+		(v = arr[i]) !== undefined; i++) {
+		!o[v] && (r.push(v), o[v] = true);
+}
+return r;
+};
+var uniqueFilter = function(arr) {
+	return arr.filter(function(elem, pos, self) {
+		return self.indexOf(elem, pos + 1) === -1;
+	});
+};
+
 // add tests
-suite.add('RegExp#test', function() {
-  /o/.test('Hello World!');
+suite.add('uniqueFor', function() {
+	uniqueFor(a);
 })
-.add('String#indexOf', function() {
-  'Hello World!'.indexOf('o') > -1;
+.add('uniqueObject', function() {
+	uniqueObject(b);
+})
+.add('uniqueFilter', function() {
+	uniqueFilter(c);
 })
 // add listeners
 .on('cycle', function(event) {
-  console.log(String(event.target));
+	console.log(String(event.target));
 })
 .on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
+	console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 // run async
-.run({ 'async': true });
+.run();
 
 // logs:
 // => RegExp#test x 4,161,532 +-0.99% (59 cycles)
 // => String#indexOf x 6,139,623 +-1.00% (131 cycles)
-// => Fastest is String#indexOftest
+// => Fastest is String#indexOf
